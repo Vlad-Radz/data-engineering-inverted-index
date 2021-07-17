a8942fb0ec15341d2271f4f337142f6ce1414bc6
Vlad <uladzislau.radziuk@nordcloud.com>
2021-07-17 09:19:13 +0200

    refactor: app to make it look nicer; remove not necessary comments; add TODOs

--- app/app.py
+++ app/app.py
@@ -1,26 +1,6 @@
 
 from pyspark.sql import functions as F
 
-# TODO: SparkSession and SparkContext
-# spark = SparkSession.builder.appName('inverted_index').getOrCreate()
-
-input_bucket = 's3://pyspark-test-vlad/'
-input_path = '/*.txt'
-
-# TODO: partition by letter - less shuffling (right?)
-# TODO: no need to parallelize - or?
-
-log_txt = sc.wholeTextFiles(input_bucket)
-one_log_txt = log_txt.mapValues(lambda k: k.split("\n"))
-#log_df = one_log_txt.toDF(['source', 'words'])
-#split_col = F.split(log_df['source'], '/')
-#log_df = log_df.withColumn('filename', split_col.getItem(3))
-#log_df.select('filename').collect()
-
-# exploded_df = log_df.withColumn("words", F.explode("words")).select(['filename', 'words'])
-one_log_txt = one_log_txt.flatMap(lambda x: [(x[0], w) for w in x[1]])
-one_log_txt = one_log_txt.map(lambda x: (x[1], x[0]))
-exploded_df = one_log_txt.reduceByKey(lambda a,b: [a, b])
 
 def flatten(_2d_list):
     flat_list = []
@@ -37,11 +17,29 @@ def flatten(_2d_list):
         flat_list.append(_2d_list)
     return flat_list
 
-exploded_df = exploded_df.map(lambda x: (x[0], flatten(x[1])))
-exploded_df = exploded_df.map(lambda x: (x[0], flatten(x[1])))  # dirty hack
-exploded_df = exploded_df.map(lambda x: (x[0], flatten(x[1])))
 
-exploded_df = exploded_df.map(lambda x: (x[0], [n.split("/")[3] for n in x[1]]))
+# TODO: SparkSession and SparkContext
+# spark = SparkSession.builder.appName('inverted_index').getOrCreate()
 
-for x in exploded_df.collect():
+# TODO: connect to S3: https://stackoverflow.com/questions/33378422/how-to-choose-an-aws-profile-when-using-boto3-to-connect-to-cloudfront
+input_bucket = 's3://pyspark-test-vlad/'
+input_path = '/*.txt'
+
+# TODO: partition by letter - less shuffling (right?)
+# TODO: no need to parallelize - or?
+
+rdd_files_contents = sc.wholeTextFiles(input_bucket)
+rdd_words = rdd_files_contents.\
+                mapValues(lambda k: k.split("\n")).\
+                flatMap(lambda x: [(x[0], w) for w in x[1]]).\
+                map(lambda x: (x[1], x[0])).\
+                reduceByKey(lambda a,b: [a, b]).\
+                map(lambda x: (x[0], flatten(x[1]))).\
+                map(lambda x: (x[0], flatten(x[1]))).\
+                map(lambda x: (x[0], flatten(x[1]))).\
+                map(lambda x: (x[0], [n.split("/")[3] for n in x[1]]))
+
+# TODO: dirty hack: flattening needs to be done 3x - function needs to be adjusted
+
+for x in rdd_words.collect():
     print(x)
